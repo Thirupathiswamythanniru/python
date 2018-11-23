cd59ab0ecf287ea7c685488c5b1e1e316319a642
Thirupathiswamythanniru <42571851+Thirupathiswamythanniru@users.noreply.github.com>
2018-11-23 18:09:38 +0530

    Create v or c.py

---
+++ v or c.py
@@ -0,0 +1,7 @@
+ch = input( "invalid")
+
+if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
+ or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+    print(ch, "invalid")
+else:
+    print(ch, "invalid")
