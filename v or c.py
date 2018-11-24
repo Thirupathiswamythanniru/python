9aa782a1e6fd4742fcf518edca937d1fba04833b
Thirupathiswamythanniru <42571851+Thirupathiswamythanniru@users.noreply.github.com>
2018-11-24 10:20:19 +0530

    Delete v or c.py

--- v or c.py
+++
@@ -1,7 +0,0 @@
-ch = input( "invalid")
-
-if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
- or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
-    print(ch, "invalid")
-else:
-    print(ch, "invalid")
