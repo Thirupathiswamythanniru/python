d99872fe07dac50ee433cd83d022d48d09cd98ab
Thirupathiswamythanniru <42571851+Thirupathiswamythanniru@users.noreply.github.com>
2018-12-07 10:59:38 +0530

    Update and rename sum of arm.c to sum of arm.py

---
+++ sum of arm.py
@@ -0,0 +1,14 @@
+def Nth_of_AP(a, d, N) : 
+  
+    # using formula to find the  
+    # Nth term t(n) = a(1) + (n-1)*d 
+    return (a + (N - 1) * d) 
+       
+   
+# Driver code 
+a = 10 # starting number 
+d = 1  # Common difference 
+N = 1 # N th term to be find 
+   
+# Display the output 
+print( " ", Nth_of_AP(a, d, N)) 
