7127e4935b707830cdbf9c348e4d682934bdc49b
Thirupathiswamythanniru <42571851+Thirupathiswamythanniru@users.noreply.github.com>
2018-12-13 19:49:57 +0530

    Update swap.py

--- swap.py
+++ swap.py
@@ -1,13 +1,8 @@
-n1 = input('')
-n2 = input('')
-
-print("", n1,end='')
-print("", n2,end='')
-
-# swapping two numbers using temporary variable
+n1,n2=map(int,input().split())
+print("",n1,end='')
+print("",n2,end='')
 temp = n1
 n1 = n2
 n2 = temp
-
 print("", n1,end='')
 print("", n2,end='')
