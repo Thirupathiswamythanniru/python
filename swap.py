b4638605806b435272fc76f5bb8c6977b971c489
Thirupathiswamythanniru <42571851+Thirupathiswamythanniru@users.noreply.github.com>
2018-12-13 19:43:57 +0530

    Create swap.py

---
+++ swap.py
@@ -0,0 +1,13 @@
+n1 = input('')
+n2 = input('')
+
+print("", n1,end='')
+print("", n2,end='')
+
+# swapping two numbers using temporary variable
+temp = n1
+n1 = n2
+n2 = temp
+
+print("", n1,end='')
+print("", n2,end='')
