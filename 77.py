cf8423b0e6e3d206bc1c5e9fe31a6ea5078e7760
Thirupathiswamythanniru <42571851+Thirupathiswamythanniru@users.noreply.github.com>
2018-12-15 12:55:01 +0530

    Create 77.py

---
+++ 77.py
@@ -0,0 +1,9 @@
+num=int(input(" "))
+count=0
+if num>0:
+    for i in range(1,num+1):
+        if (num%i==0):
+          if(i!=num):
+             print ((i),end=' ')
+          else:
+             print((i),end='')
